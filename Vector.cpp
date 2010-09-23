029e2107aebf66bd28e0fde36fb4756435075318
Andreas Tarandi <torandi@gmail.com>
2010-09-23 14:27:13 +0200

    Moved out init of data

--- Vector.cpp
+++ Vector.cpp
@@ -1,13 +1,15 @@
 #include "Vector.h"
 
+#define INIT_DATA(size) _data = new unsigned int[size];
+
 Vector::Vector(size_t size) : _size(size)  {
-	_data = new unsigned int[size];
+	INIT_DATA(size);
 	memset(_data,0,sizeof(unsigned int)*size);
 }
 
 Vector::Vector(const Vector &vector) : _size(vector._size) {
-    _data = new unsigned int[vector._size];
-    memcpy(_data, vector._data, sizeof(unsigned int)*_size);
+	INIT_DATA(size);
+   memcpy(_data, vector._data, sizeof(unsigned int)*_size);
 }
 
 Vector::~Vector() {
