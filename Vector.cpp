e03aaf6151f7ae9d07944a8a320148990348fec9
mikolan <mmikkola@kth.se>
2010-09-23 14:16:01 +0200

    test.cpp

--- Vector.cpp
+++ Vector.cpp
@@ -1,5 +1,4 @@
 #include "Vector.h"
-#include <cstring>
 
 Vector::Vector(size_t size) : _size(size)  {
 	_data = new unsigned int[size];
---
+++ test.cpp
@@ -0,0 +1,8 @@
+#include "Vector.h"
+
+int main() {
+    Vector vector10(10);
+    Vector vector0(0);
+    
+    Vector vectorCopy(vector10);
+}
