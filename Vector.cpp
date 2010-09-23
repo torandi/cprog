ef2342788b7d658a8ca5705eaf0165d9e777f191
Andreas Tarandi <torandi@gmail.com>
2010-09-23 14:09:16 +0200

    Init data to 0

--- Vector.cpp
+++ Vector.cpp
@@ -1,7 +1,9 @@
 #include "Vector.h"
+#include <cstring>
 
 Vector::Vector(size_t size) : _size(size)  {
 	_data = new unsigned int[size];
+	memset(_data,0,sizeof(unsigned int)*size);
 }
 
 Vector::~Vector() {
