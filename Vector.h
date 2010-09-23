4490ed3f1d3667dbc7b3db02d85f18ffba98c705
mikolan <mmikkola@kth.se>
2010-09-23 15:55:46 +0200

    Added method declarations to Vector.h

--- Vector.h
+++ Vector.h
@@ -6,13 +6,20 @@ template <class T>
 class Vector {
 	T** _data;
 	size_t _size;
-
+	size_t _elements;
+	
 public:
 	explicit Vector(size_t size);
 	Vector(const Vector<T> &vector);
 	~Vector();
 	Vector<T>& operator=(const Vector<T> &vector);
 	T& operator[](unsigned int index) const;
+	void push_back(const T& t);
+	void insert(size_t i, const T& t);
+	const T& erase(size_t i);
+	void clear();
+	size_t size() const;
+	void sort(bool ascending = true);
 };
 
 #endif
