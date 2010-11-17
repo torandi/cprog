e7f366999b63d1ce95ee0c63347f5152f0b7acc0
Andreas Tarandi <torandi@gmail.com>
2010-11-17 12:40:58 +0100

    Ansidatasdtsdfclol

--- labb2/ansi_date.h
+++ labb2/ansi_date.h
@@ -9,18 +9,24 @@ namespace lab2 {
 	class AnsiDate : public Date {
 		static const std::string weekdays[7];
 		static const std::string months[12];
-		static const int days_per_month[12];
 		protected:
 			AnsiDate();
 			virtual ~AnsiDate();
+			static const int days_per_month[12];
 
 
 		public:
-			virtual int days_per_week() const { return 7; };
-			virtual int months_per_year() const { return 12; };
-			virtual int days_this_month() const;
-			virtual std::string week_day_name() const;
-			virtual std::string month_name() const;
+			virtual const int days_per_week() const { return 7; };
+			virtual const int months_per_year() const { return 12; };
+			virtual const std::string week_day_name() const;
+			virtual const std::string month_name() const;
+
+			virtual Date& operator=(const Date &date);
+			virtual bool operator==(const Date &date) const;
+			bool operator!=(const Date &date) const;
+
+			virtual Date &operator+=(const int n);
+			virtual Date &operator-=(const int n);
 	};
 };
 #endif
